978e7a83b65a460b7af3c7deec43231d7806b1bc
junah201 <junah.dev@gmail.com>
2023-03-29 22:13:03 +0900

    Init commit

---
+++ common/const.py
@@ -0,0 +1,5 @@
+import os
+
+
+PREFIX = os.environ.get("PREFIX", "!")
+TOKEN = os.environ.get("TOKEN")
---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from common import const
+
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=const.PREFIX,
+            intents=discord.Intents.all(),
+            sync_command=True,
+        )
+
+        self.initial_extension = [
+            "Cogs.test",
+        ]
+
+    async def setup_hook(self):
+        for ext in self.initial_extension:
+            await self.load_extension(ext)
+
+        await bot.tree.sync()
+
+    async def on_ready(self):
+        print(f"DISCORD BOT : {self.user}")
+        print(
+            f"Discord Version : {discord.__version__}")
+        print(f"On ready")
+
+
+if __name__ == '__main__':
+    bot = Bot()
+    bot.run(const.TOKEN)
