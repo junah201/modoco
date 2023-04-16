ef28b62c51b20d9ea2fcd8beacef11f9669fac13
junah201 <junah.dev@gmail.com>
2023-04-16 11:42:18 +0900

    Fix: 채널 입장 권한이 없던 일반 유저의 권한 문제 해결

--- Cogs/voice_channel_handler.py
+++ Cogs/voice_channel_handler.py
@@ -10,10 +10,12 @@ class VoiceChannelHandler(commands.Cog):
     def __init__(self, bot: discord.Client):
         self.bot = bot
         self.auto_generator_channel: Optional[discord.VoiceChannel] = None
+        self.server_authentication_role: Optional[discord.Role] = None
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.auto_generator_channel: discord.VoiceChannel = await utils.get_channel_by_id(self.bot, config.AUTO_VOICE_CHANNEL_GENERATOR_CHANNEL_ID)
+        self.server_authentication_role = await utils.get_role_by_guild(self.auto_generator_channel.guild, config.SERVER_AUTHENTICATION_ROLE_ID)
 
     @commands.Cog.listener()
     async def on_voice_state_update(
@@ -33,6 +35,7 @@ class VoiceChannelHandler(commands.Cog):
                 overwrites={
                     member.guild.default_role: discord.PermissionOverwrite(manage_channels=False, connect=False, mute_members=False, kick_members=False, deafen_members=False),
                     member: discord.PermissionOverwrite(manage_channels=True, connect=True, mute_members=False, kick_members=False, deafen_members=False),
+                    self.server_authentication_role: discord.PermissionOverwrite(manage_channels=False, connect=True, view_channel=True),
                 }
             )
             await member.move_to(new_voice_channel)
